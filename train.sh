c4f345acbc15977e01443c82b0c750e3ab72a3a9
Eashwar <eashwar.s.erahan@gmail.com>
2021-07-26 22:15:37 +0200

    config update with eval data

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model bisenet_v1_g6
-python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v1
-python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v4
+python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v1_se1
+python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v1_se2
+python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v1_se3
+python -m torch.distributed.launch --nproc_per_node=1 train/train.py --model fanet18_v4_se1
